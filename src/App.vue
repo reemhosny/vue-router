1a6d3c9065098b8ecd47cb0073a0d9c0d7eae83d
reemhosny <reem.hosny@waffarha.com>
2024-08-03 19:50:33 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Home from '../src/views/Home.vue';
+</script>
+
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/brazil">Brazil</router-link>
+  </div>
+  <div class="container">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/views/Brazil.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="brazil">
+    <h1>Brazil</h1>
+  </div>
+</template>
